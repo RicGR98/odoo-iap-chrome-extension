2d01aa06bb9e8279baacfd34cba8cc967120bee9
Ricardo Gomes Rodrigues <rigr@odoo.com>
2025-10-20 15:16:56 +0200

    Switch to textarea which is simplier code-wise and even UX-wise

--- settings.js
+++ settings.js
@@ -1,125 +1,26 @@
 window.addEventListener('DOMContentLoaded', () => {
+    const configParamsShowCheckboxEl = document.getElementById('config-parameters-show-checkbox');
+    const iapAccountsShowCheckboxEl = document.getElementById('iap-accounts-show-checkbox');
+
+    const configParamsStatusMessageOkEl = document.getElementById('config-parameters-status-message-ok');
+    const configParamsStatusMessageNokEl = document.getElementById('config-parameters-status-message-nok');
+    const iapAccountsStatusMessageOkEl = document.getElementById('iap-accounts-status-message-ok');
+    const iapAccountsStatusMessageNokEl = document.getElementById('iap-accounts-status-message-nok');
+
+    const configParamsMappingEl = document.getElementById('config-parameters-mapping');
+    const iapAccountsMappingEl = document.getElementById('iap-accounts-mapping');
+
+    const resetButtonEl = document.getElementById('reset-button');
+    const saveButtonEl = document.getElementById('save-button');
+
     let settings = {};
 
-    function addOption(mapping, key, optionShortName, optionValue){
-        if (optionShortName === "") {
-            return;
-        }
-        mapping[key] = {
-            ...mapping[key],
-            [optionShortName]: optionValue,
-        }
-        renderSettings();
-    }
-
-    function deleteOption(mapping, key, optionShortName){
-        delete mapping[key][optionShortName];
-        renderSettings();
-    }
-
-    function renderMapping(mappingContainer, mapping){
-        mappingContainer.innerHTML = "";
-
-        for (let [key, options] of Object.entries(mapping)) {
-            options = Object.entries(options).sort()
-            options.push(["", ""]);  // Adds a new empty line that can be filled in to add a new option
-
-            const keyContainer = document.createElement('div');
-            keyContainer.className = 'key-container';
-            mappingContainer.appendChild(keyContainer);
-
-            const keyLabel = document.createElement('div');
-            keyLabel.className = 'mapping-key';
-            keyLabel.textContent = key;
-            keyContainer.appendChild(keyLabel);
-
-            const showLabel = document.createElement('label');
-            showLabel.textContent = "Show";
-            keyContainer.appendChild(showLabel);
-
-            const showKeyCheckbox = document.createElement('input');
-            showKeyCheckbox.type = "checkbox";
-            showKeyCheckbox.checked = mapping[key]["show"];
-            showKeyCheckbox.addEventListener('change', (e) => mapping[key]["show"] = e.target.checked);
-            showLabel.appendChild(showKeyCheckbox);
-
-            const deleteKeyButton = document.createElement('button');
-            deleteKeyButton.textContent = "Delete mapping";
-            deleteKeyButton.addEventListener('click', () => {
-                delete mapping[key];
-                renderSettings();
-            });
-            keyContainer.appendChild(deleteKeyButton);
-
-            for (const [optionShortName, optionValue] of options) {
-                if (optionShortName === "show"){
-                    continue;
-                }
-
-                const optionContainer = document.createElement('div');
-                optionContainer.className = 'option-container';
-                keyContainer.appendChild(optionContainer);
-
-                const optionShortNameInput = document.createElement('input');
-                const optionValueInput = document.createElement('input');
-
-                optionShortNameInput.type = 'text';
-                optionShortNameInput.value = optionShortName;
-                optionShortNameInput.placeholder = "Short name";
-                optionShortNameInput.addEventListener('change', () => mapping[key][optionShortNameInput.value] = optionValueInput.value);
-                optionContainer.appendChild(optionShortNameInput);
-
-                optionValueInput.type = 'text';
-                optionValueInput.value = optionValue;
-                optionValueInput.placeholder = "Value";
-                optionValueInput.addEventListener('change', () => mapping[key][optionShortNameInput.value] = optionValueInput.value);
-                optionContainer.appendChild(optionValueInput);
-
-                if (optionShortName === ""){
-                    const addButton = document.createElement('button');
-                    addButton.textContent = "Add option";
-                    addButton.addEventListener('click', () => addOption(mapping, key, optionShortNameInput.value, optionValueInput.value));
-                    optionContainer.appendChild(addButton);
-                } else {
-                    const deleteButton = document.createElement('button');
-                    deleteButton.textContent = "Delete option";
-                    deleteButton.addEventListener('click', () => deleteOption(mapping, key, optionShortNameInput.value));
-                    optionContainer.appendChild(deleteButton);
-                }
-            }
-        }
-    }
-
     function renderSettings() {
-        // Config Params
-        const configParamsShowCheckbox = document.getElementById('config-parameters-show-checkbox')
-        configParamsShowCheckbox.checked = settings.configParamsShow;
-        configParamsShowCheckbox.addEventListener('change', () => {
-            settings["configParamsShow"] = configParamsShowCheckbox.checked;
-        });
-        document.getElementById('config-parameters-new-button').addEventListener('click', () => {
-            settings['configParamsMapping'][document.getElementById('config-parameters-new-input').value] = {
-                "show": true,
-            };
-            document.getElementById('config-parameters-new-input').value = '';
-            renderSettings();
-        });
-        renderMapping(document.getElementById('config-parameters-mapping-container'), settings.configParamsMapping);
+        configParamsShowCheckboxEl.checked = settings["configParamsShow"];
+        iapAccountsShowCheckboxEl.checked = settings["iapAccountsShow"];
 
-        // IAP Accounts
-        const iapAccountsShowCheckbox = document.getElementById('iap-accounts-show-checkbox')
-        iapAccountsShowCheckbox.checked = settings.configParamsShow;
-        iapAccountsShowCheckbox.addEventListener('change', () => {
-            settings["iapAccountsShow"] = iapAccountsShowCheckbox.checked;
-        });
-        document.getElementById('iap-accounts-new-button').addEventListener('click', () => {
-            settings['iapAccountsMapping'][document.getElementById('iap-accounts-new-input').value] = {
-                "show": true,
-            };
-            document.getElementById('iap-accounts-new-input').value = '';
-            renderSettings();
-        });
-        renderMapping(document.getElementById('iap-accounts-mapping-container'), settings.iapAccountsMapping);
+        configParamsMappingEl.value = JSON.stringify(settings["configParamsMapping"], null, 4);
+        iapAccountsMappingEl.value = JSON.stringify(settings["iapAccountsMapping"], null, 4);
     }
 
     function resetSettings() {
@@ -133,6 +34,20 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    function onInput(settingKey, mappingEl, statusMessageOkEl, statusMessageNokEl) {
+        try {
+            settings[settingKey] = JSON.parse(mappingEl.value);
+            statusMessageOkEl.style.display = "block";
+            statusMessageNokEl.style.display = "none";
+            saveButtonEl.disabled = false;
+        } catch (e) {
+            console.error(e);
+            statusMessageOkEl.style.display = "none";
+            statusMessageNokEl.style.display = "block";
+            saveButtonEl.disabled = true;
+        }
+    }
+
     function setup(){
         chrome.storage.sync.get('settings', (result) => {
             if (result.settings) {
@@ -143,13 +58,38 @@ window.addEventListener('DOMContentLoaded', () => {
             renderSettings();
         });
 
-        document.getElementById('reset-button').addEventListener('click', () => {
+        resetButtonEl.addEventListener('click', () => {
             resetSettings();
         });
-
-        document.getElementById('save-button').addEventListener('click', () => {
+        saveButtonEl.addEventListener('click', () => {
             saveSettings();
         });
+
+        configParamsShowCheckboxEl.addEventListener('change', () => {
+            settings["configParamsShow"] = configParamsShowCheckboxEl.checked;
+        });
+        iapAccountsShowCheckboxEl.addEventListener('change', () => {
+            settings["iapAccountsShow"] = iapAccountsShowCheckboxEl.checked;
+        });
+
+        configParamsMappingEl.addEventListener('input', () => {
+            onInput("configParamsMapping", configParamsMappingEl, configParamsStatusMessageOkEl, configParamsStatusMessageNokEl);
+        });
+        iapAccountsMappingEl.addEventListener('input', () => {
+            onInput("iapAccountsMapping", iapAccountsMappingEl, iapAccountsStatusMessageOkEl, iapAccountsStatusMessageNokEl);
+        });
+
+        document.querySelectorAll('textarea').forEach(el => el.addEventListener('keydown', function(e) {
+            if (e.key == 'Tab') {
+                e.preventDefault();
+                var start = this.selectionStart;
+                var end = this.selectionEnd;
+
+                this.value = this.value.substring(0, start) + "    " + this.value.substring(end);
+
+                this.selectionStart = this.selectionEnd = start + 4;
+            }
+        }));
     }
 
     setup();
